2c5b86309536df2930ba9cb6c72303d58712578f
Michael <michamaxe@gmail.com>
2022-10-26 20:32:12 +0200

    fix pylint

--- app/models.py
+++ app/models.py
@@ -2,10 +2,60 @@
 import enum
 import uuid
 
-from sqlalchemy import Column, String, Boolean, DateTime, func, Enum, Integer
+from sqlalchemy import Column, String, Boolean, DateTime, func, Enum, Integer, TypeDecorator, CHAR
 
 from .database import Base
-from .utils import GUID
+
+
+class GUID(TypeDecorator):
+	"""Platform-independent GUID type.
+	Uses PostgreSQL's UUID type, otherwise uses
+	CHAR(32), storing as stringified hex values.
+	"""
+	impl = CHAR
+
+	cache_ok = True
+
+	def load_dialect_impl(self, dialect):
+		if dialect.name == 'postgresql':
+			return dialect.type_descriptor(uuid.UUID())
+
+		return dialect.type_descriptor(CHAR(32))
+
+	def process_bind_param(self, value, dialect):
+		if value is None:
+			return value
+
+		if dialect.name == 'postgresql':
+			return str(value)
+
+		if not isinstance(value, uuid.UUID):
+			return f'{uuid.UUID(value).int:32x}'
+
+		# hex string
+		return f'{value.int:32x}'
+
+	def process_result_value(self, value, dialect):
+		if value is None:
+			return value
+
+		if not isinstance(value, uuid.UUID):
+			value = uuid.UUID(value)
+
+		return value
+
+	def process_literal_param(self, value, dialect):
+		"""
+			Receive a literal parameter value to be rendered inline within
+			a statement.
+		"""
+
+	@property
+	def python_type(self):
+		"""
+			Return the Python type object expected to be returned
+			by instances of this type, if known.
+		"""
 
 
 class GenderChoices(enum.Enum):
